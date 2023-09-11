83b16d4ee26a3a395ff29962641a0e823edfd9ab
slowteady <vbn0213@gmail.com>
2023-09-11 17:24:30 +0900

    feat: 데이터 변환을 위한 타입 인터페이스 추가

--- src/types/chartData.d.ts
+++ src/types/chartData.d.ts
@@ -1,9 +1,19 @@
 export interface ChartData {
   id: string;
-  value_area: number;
-  value_bar: number;
+  value_area: string;
+  value_bar: string;
 }
 
 export interface ChartObj extends ChartData {
   date: string;
+}
+
+export interface ChangeFormatChartData {
+  id: string;
+  value_area: number;
+  value_bar: number;
+}
+
+export interface ChartObjParams {
+  [key: string]: ChangeFormatChartData;
 }
