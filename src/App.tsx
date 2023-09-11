38eca15983040f24d106fc01c65bf10f099ac4ca
slowteady <vbn0213@gmail.com>
2023-09-11 17:23:42 +0900

    design: 중앙 정렬, 최대 크기로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 import Router from './router/Router';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Router />
+      <FullWrap>
+        <Router />
+      </FullWrap>
     </BrowserRouter>
   );
-}
+};
+
+const FullWrap = styled.main`
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
 
 export default App;
