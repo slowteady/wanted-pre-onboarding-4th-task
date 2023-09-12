1d67e85c40c31e3675a702992b62088c83a1458a
slowteady <vbn0213@gmail.com>
2023-09-12 22:37:58 +0900

    feat: 중복값 상수처리, 그래프 클릭 시 필터링 처리, 버튼 클릭 시 필터링 처리

--- src/components/charts/Chart.tsx
+++ src/components/charts/Chart.tsx
@@ -1,8 +1,36 @@
-import { Area, Bar, CartesianGrid, ComposedChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Area,
+  Bar,
+  CartesianGrid,
+  Cell,
+  ComposedChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
+import { CategoricalChartState } from 'recharts/types/chart/generateCategoricalChart';
+import { useFilter } from '../../state/FilterContext';
 import { ChartObj } from '../../types/chartData';
 import CustomTooltip from './custom/CustomTooltip';
 
 const Chart = ({ data }: { data: ChartObj[] }) => {
+  const { filterIds, setFilterIds } = useFilter();
+
+  const addFilter = (event: CategoricalChartState | null) => {
+    if (event && event.activePayload) {
+      const barPayloadId = event.activePayload[0].payload.id;
+      if (barPayloadId) {
+        setFilterIds(
+          filterIds.includes(barPayloadId)
+            ? filterIds.filter((filterId) => filterId !== barPayloadId)
+            : [...filterIds, barPayloadId]
+        );
+      }
+    }
+  };
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <ComposedChart
@@ -12,13 +40,14 @@ const Chart = ({ data }: { data: ChartObj[] }) => {
           right: 30,
           left: 30
         }}
+        onClick={addFilter}
       >
         <XAxis dataKey="date" style={{ fontSize: '0.8rem' }} minTickGap={30} />
         <YAxis
           yAxisId="left"
           width={100}
           tickMargin={5}
-          label={{ value: 'value_bar', angle: 270, position: 'left' }}
+          label={{ value: BAR_DATA_KEY, angle: 270, position: 'left' }}
           tickFormatter={(value) => parseFloat(value).toFixed(2)}
         />
         <YAxis
@@ -27,17 +56,24 @@ const Chart = ({ data }: { data: ChartObj[] }) => {
           width={100}
           tickMargin={5}
           domain={[0, 200]}
-          label={{ value: 'value_area', angle: 90, position: 'right' }}
+          label={{ value: AREA_DATA_KEY, angle: 90, position: 'right' }}
           tickFormatter={(value) => parseFloat(value).toFixed(2)}
         />
         <Tooltip content={<CustomTooltip />} />
         <Legend />
         <CartesianGrid strokeDasharray="3 3" />
-        <Bar dataKey="value_bar" yAxisId="left" barSize={10} fill="#c5c5f2" />
-        <Area yAxisId="right" dataKey="value_area" fill="#b1b1bde4" stroke="#b1b1bde4" />
+        <Bar dataKey={BAR_DATA_KEY} yAxisId="left" barSize={10} fill="#c5c5f2">
+          {data.map((entry, index) => (
+            <Cell key={index} fill={filterIds.includes(entry.id) ? '#5f5fac' : '#c5c5f2'} />
+          ))}
+        </Bar>
+        <Area yAxisId="right" dataKey={AREA_DATA_KEY} fill="#b1b1bde4" stroke="#b1b1bde4" />
       </ComposedChart>
     </ResponsiveContainer>
   );
 };
+
+const BAR_DATA_KEY = 'value_bar';
+const AREA_DATA_KEY = 'value_area';
 
 export default Chart;
